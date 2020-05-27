0d9c12dc3277e16517e7d910e8a1980f37ebca79
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-05-27 18:38:23 -0400

    I changed a nav component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,32 @@
 import React from "react";
- import styled from "@emotion/styled";
- 
+import styled from "@emotion/styled";
 
- const NavBar = (props) => {
-     const NavList = styled.nav`
-     `
-     const Ul = styled.ul`
-         display: flex;
-         justify-content: space-between;
-         align-items: center;
-     `;
-     const List = styled.li`
-         list-style: none;
-         font-size: 18px;
-         padding-right: 15px;
-         cursor: pointer;
-         color: white;
-         text-transform: capitalize;
-     `;
-     return (
-       <NavList>
-         <Ul>
-           <List>home</List>
-           <List>about</List>
-           <List>projects</List>
-           <List>sign in</List>
-           <List>sign up</List>
-         </Ul>
-       </NavList>
-     );
- }
-  
- export default NavBar;
+const NavBar = (props) => {
+  const NavList = styled.nav``;
+  const Ul = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+  const List = styled.li`
+    list-style: none;
+    font-size: 18px;
+    padding-right: 15px;
+    cursor: pointer;
+    color: white;
+    text-transform: capitalize;
+  `;
+  return (
+    <Navlist>
+      <Ul>
+        <List>home</List>
+        <List>about</List>
+        <List>projects</List>
+        <List>sign in</List>
+        <List>sign up</List>
+      </Ul>
+    </Navlist>
+  );
+};
+
+export default NavBar;
