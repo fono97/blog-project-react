8f267ea2e7849c14dc2b2a3fd4986a0672bb669c
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-05-26 23:19:00 -0400

    I added images and my component folder for all my component

---
+++ src/components/blog.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+ const Site = (props) =>{
+     const Blog = styled.div`
+       .blog {
+         display: grid;
+         grid-template-columns: 1fr 1fr 1fr 1fr;
+         grid-template-rows: 100px auto auto;
+         grid-template-areas:
+           "header header header header"
+           "main main main sidebar"
+           "footer footer footer footer";
+       }
+     `
+     return (
+                <Blog>
+                    <header>
+                        <div className ="header-title">
+                            <div className="logo-nav">
+                                <div className="logo">
+                                    <img src="./images/logo.png" alt="computer-science-logo" />
+                                </div>
+                                <p className="title-blog">web development projects</p>
+                            </div>
+                            <nav className="navbar">
+                                <ul>
+                                    <li>home</li>
+                                    <li>about</li>
+                                    <li>projects</li>
+                                    <li>sign in</li>
+                                    <li>sign up</li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </header>
+                    <main>
+                        <section>
+                            <h2>These are all projects</h2>
+                            <div className="all-posts">
+                                <article>
+                                    <div className="post">
+                                        <p>post</p>
+                                        <div>
+                                            <p>view post</p>
+                                        </div>
+                                    </div>
+                                    <p>Title</p>
+                                </article>
+                            </div>
+                        </section>
+                    </main>
+
+                    <aside>
+                        <p>he</p>
+                    </aside>
+
+                    <footer>
+                        <p>this is my footer</p>
+                    </footer>
+                </Blog>
+     )
+     
+ }
+ export default Site;
